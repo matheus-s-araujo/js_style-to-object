370e32db91799c571465a5fcff9b535c25c8dfcb
Matheus Sousa <theus.sousaa@gmail.com>
2025-09-17 13:49:49 -0300

    finished project

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const linesArr = sourceString.split(';');
+  const objPropertiesArr = [];
+  const finalObject = {};
+
+  // Catching the object properties and their values
+  for (let i = 0; i < linesArr.length; i++) {
+    linesArr[i] = linesArr[i].trim();
+
+    if (linesArr[i]) {
+      objPropertiesArr.push(linesArr[i].split(':'));
+    }
+  }
+
+  for (let j = 0; j < objPropertiesArr.length; j++) {
+    finalObject[objPropertiesArr[j][0].trim()] = objPropertiesArr[j][1].trim();
+  }
+
+  return finalObject;
 }
 
 module.exports = convertToObject;
